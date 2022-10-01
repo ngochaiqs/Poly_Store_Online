1e2c8f4aa33de41b9c2b60d10e10c19b38c52ca9
Luong Ngoc Hai <luongngochaiqs@gmail.com>
2022-10-01 13:44:55 +0700

    Test phân nhánh

--- app/src/main/java/com/poly_store/activity/AoThunActivity.java
+++ app/src/main/java/com/poly_store/activity/AoThunActivity.java
@@ -11,5 +11,6 @@ public class AoThunActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ao_thun);
+        //AoThunActivity thay cho DienThoaiActivity
     }
 }
