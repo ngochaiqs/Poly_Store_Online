b364df58ab3c8ab4c18685f3c9d816d347b5120c
LAPTOP-CQ4KUNBU\Asus-FPT <phamvanhauvtv114@gmail.com>
2022-10-07 21:47:57 +0700

    18

--- app/src/main/java/com/poly_store/utils/Utils.java
+++ app/src/main/java/com/poly_store/utils/Utils.java
@@ -1,11 +1,12 @@
 package com.poly_store.utils;
 
 import com.poly_store.model.GioHang;
+import com.poly_store.model.NguoiDung;
 
 import java.util.List;
 
 public class Utils {
     public static final String BASE_URL = "https://luongngochai.000webhostapp.com/";
-
+    public static NguoiDung nguoidung_current = new NguoiDung();
     public static List<GioHang> manggiohang;
 }
